fa5eebbae312382a46ab7fd6df5058d740236927
Victor Chan <vchan@atlassian.com>
2022-12-10 07:01:28 +1100

    day09

--- day09/index.rb
+++ day09/index.rb
@@ -9,32 +9,27 @@ end
 # old pos U/D/L/R -> tail go in that direction
 # old pos diagonal -> tail goes diagonal
 class Rope
-  attr_accessor :head, :tail, :trail
-  def initialize
+  attr_accessor :head, :tail, :trail, :trailing
+
+  def initialize(num_tails)
     @head = [0, 0]
-    @tail = [0, 0]
+    @trailing = num_tails > 0 && Rope.new(num_tails - 1)
     @trail = Set[[0, 0]]
     @rel = { "above" => "U", "below" => "D", "left" => "L", "right" => "R" }
   end
 
-  def get_rel()
+  def get_rel
     rel = []
-    rel << @rel["left"] if @head.first < @tail.first
-    rel << @rel["right"] if @head.first > @tail.first
-    rel << @rel["above"] if @head.last < @tail.last
-    rel << @rel["below"] if @head.last > @tail.last
+    tail = @trailing.head
+    rel << @rel["left"] if @head.first < tail.first
+    rel << @rel["right"] if @head.first > tail.first
+    rel << @rel["above"] if @head.last < tail.last
+    rel << @rel["below"] if @head.last > tail.last
     rel
   end
 
   def touching?(p1, p2)
     (p1.first - p2.first).abs < 2 && (p1.last - p2.last).abs < 2
-  end
-
-  def move_tail(d, rel)
-    return if rel.empty?
-    return if touching?(@head, @tail)
-    return move!(@tail, d) if rel.length == 1
-    return rel.each { |dir| move!(@tail, dir) }
   end
 
   def move!(point, d)
@@ -44,32 +39,53 @@ class Rope
     point[0] += 1 if d == "R"
   end
 
-  def move_head(d)
-    rel = get_rel
-    move!(@head, d)
-    move_tail(d, rel)
-    @trail << @tail[0..-1]
+  def moves(d, n)
+    n.times do |_|
+      moves_head([d])
+      commit
+    end
   end
 
-  def moves(d, n)
-    n.times { |_| move_head(d) }
+  def moves_head(dirs)
+    dirs.each { |d| move!(@head, d) }
+    move_rest if @trailing
+  end
+
+  def move_rest
+    return if touching?(@head, @trailing.head)
+    # depends on new relative position. if new relative position is right/left/top/bottom, then only move in that direction, else move diagonal
+    return @trailing.moves_head(get_rel)
+  end
+
+  def get_tail
+    tail = self.trailing
+    tail = tail.trailing while tail.trailing
+    tail
+  end
+
+  def commit
+    return @trailing.commit if @trailing
+    @trail << @head[0..-1]
   end
 end
 
 def part1(input)
-  rope = Rope.new
+  rope = Rope.new(1)
   input.each { |(d, n)| rope.moves(d, n) }
-  rope.trail.length
+  rope.get_tail.trail.length
 end
 
 def part2(input)
-  input
+  rope = Rope.new(9)
+  input.each { |(d, n)| rope.moves(d, n) }
+  rope.get_tail.trail.length
 end
 
 puts "Part 1"
-p part1(parse("data.txt")) if part1(parse("sample.txt")) == 13
+p part1(parse("sample.txt"))
+p part1(parse("data.txt"))
 
 puts
 puts "Part 2"
-# p part2(parse("sample.txt"))
-# p part2(parse("data.txt"))
+p part2(parse("sample2.txt"))
+p part2(parse("data.txt"))
